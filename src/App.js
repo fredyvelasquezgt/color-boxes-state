c7fa094949c40c89e4d902132c1dddc6ee29b7a0
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-12 12:08:57 -0600

    finished box logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import BoxContainer from "./BoxContainer";
+
 import "./App.css";
+import Box from "./Box";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <BoxContainer />
+        <Box />
       </div>
     );
   }
--- src/Box.js
+++ src/Box.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
+import './Box.css'
+import {choice} from './helpers'
 
 class Box extends Component {
+    static defaultProps = {
+        allColors: ['purple', 'magenta', 'violet', 'pink']
+    }
     constructor(props) {
         super(props)
-        this.state = {color: 'purple'}
+        this.state = {color: choice(this.props.allColors)}
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    pickColor() {
+        let newColor; 
+        do{
+           newColor = choice(this.props.allColors);
+        } while (newColor === this.state.color)
+
+        this.setState({color: newColor})
     }
 
     handleClick() {
-        
+        this.pickColor()
     }
 
     render() {
